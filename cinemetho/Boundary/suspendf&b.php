1ddf36baf874b90a053be28e1942f916f68fab53
dededaf <132619826+dededaf@users.noreply.github.com>
2023-05-14 01:46:25 +0800

    Add files via upload

--- cinemetho/Boundary/suspendf&b.php
+++ cinemetho/Boundary/suspendf&b.php
@@ -1,13 +1,13 @@
 <!-- need to edit -->
 <?php
-include_once("../Controller/suspendUserAccountCtl.php");
+include_once("../Controller/suspendUserAccountCtl.php"); //need to change
 
-if (isset($_GET['user_id'])) {
-    $user_id = $_GET['user_id'];
+if (isset($_GET['combo_id'])) {
+    $combo_id = $_GET['combo_id'];
 
     // Set user status to "Suspended"
-    $userAccountCtl = new suspendUserAccountCtl();
-    $userAccountCtl->suspendUserAccount($user_id);
+    $userAccountCtl = new suspendUserAccountCtl(); //need to change with controller
+    $userAccountCtl->suspendf&b($combo_id);
 
     // Redirect to user admin page
     header("Location: ../Boundary/manageUser.php");
