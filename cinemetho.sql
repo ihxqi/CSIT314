ef6acef5b0afb8c3f70ab621e95594fc4e8f2eb8
elainist <elainenkx@gmail.com>
2023-05-15 21:18:45 +0800

    Update cinemetho.sql

--- cinemetho.sql
+++ cinemetho.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2023 at 05:20 AM
+-- Generation Time: May 15, 2023 at 03:17 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -31,17 +31,19 @@ CREATE TABLE `booking` (
   `booking_id` int(11) NOT NULL,
   `userBooked` int(10) NOT NULL,
   `QRCode` varchar(100) NOT NULL,
-  `bookingDate` date NOT NULL
+  `bookingDate` date NOT NULL,
+  `seatNo` int(10) NOT NULL,
+  `roomNo` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`booking_id`, `userBooked`, `QRCode`, `bookingDate`) VALUES
-(1, 26, '../Images/Scantopay.png', '2023-05-09'),
-(2, 26, '../Images/Scantopay.png', '2023-05-01'),
-(3, 26, '../Images/Scantopay.png', '2023-05-01');
+INSERT INTO `booking` (`booking_id`, `userBooked`, `QRCode`, `bookingDate`, `seatNo`, `roomNo`) VALUES
+(1, 26, '../Images/Scantopay.png', '2023-05-09', 1, 1),
+(2, 26, '../Images/Scantopay.png', '2023-05-01', 3, 2),
+(3, 26, '../Images/Scantopay.png', '2023-05-01', 4, 2);
 
 -- --------------------------------------------------------
 
@@ -50,18 +52,21 @@ INSERT INTO `booking` (`booking_id`, `userBooked`, `QRCode`, `bookingDate`) VALU
 --
 
 CREATE TABLE `cinema` (
-  `RoomNo` int(11) NOT NULL,
-  `Capacity` varchar(3) NOT NULL,
+  `roomNo` int(11) NOT NULL,
+  `roomCapacity` varchar(3) NOT NULL,
   `movieShowtime` varchar(11) NOT NULL,
-  `roomStatus` varchar(100) NOT NULL
+  `roomStatus` varchar(100) NOT NULL,
+  `movieShowDate` date NOT NULL,
+  `movie_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cinema`
 --
 
-INSERT INTO `cinema` (`RoomNo`, `Capacity`, `movieShowtime`, `roomStatus`) VALUES
-(1, '40', '7pm', 'Active');
+INSERT INTO `cinema` (`roomNo`, `roomCapacity`, `movieShowtime`, `roomStatus`, `movieShowDate`, `movie_id`) VALUES
+(1, '40', '7pm', 'Active', '2023-05-09', 1),
+(2, '30', '6pm', 'Active', '2023-05-01', 2);
 
 -- --------------------------------------------------------
 
@@ -92,19 +97,19 @@ INSERT INTO `combobooked` (`comboBooked_id`, `booking_id`, `comboQuantity`, `com
 --
 
 CREATE TABLE `fnb` (
-  `ComboID` int(10) NOT NULL,
-  `ComboName` varchar(200) NOT NULL,
-  `ComboImg` varchar(200) NOT NULL,
-  `ComboPrice` int(50) NOT NULL,
-  `ComboQuantity` int(2) NOT NULL,
-  `ComboStatus` varchar(100) NOT NULL
+  `comboID` int(10) NOT NULL,
+  `comboName` varchar(200) NOT NULL,
+  `comboImg` varchar(200) NOT NULL,
+  `comboPrice` int(50) NOT NULL,
+  `comboQuantity` int(2) NOT NULL,
+  `comboStatus` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `fnb`
 --
 
-INSERT INTO `fnb` (`ComboID`, `ComboName`, `ComboImg`, `ComboPrice`, `ComboQuantity`, `ComboStatus`) VALUES
+INSERT INTO `fnb` (`comboID`, `comboName`, `comboImg`, `comboPrice`, `comboQuantity`, `comboStatus`) VALUES
 (1, 'Combo A: Finger Snacks', '../cinemetho/Images/combo1.jpg', 14, 50, 'Active'),
 (2, 'Combo B: Couple snacks', '../cinemetho/Images/combo2.jpg', 30, 20, 'Active'),
 (3, 'Combo C: Popcorns\r\n\r\n', '../cinemetho/Images/combo3.jpg', 10, 30, 'Active');
@@ -116,31 +121,30 @@ INSERT INTO `fnb` (`ComboID`, `ComboName`, `ComboImg`, `ComboPrice`, `ComboQuant
 --
 
 CREATE TABLE `movie` (
-  `MovieID` int(10) NOT NULL,
-  `MovieTitle` varchar(200) NOT NULL,
-  `MovieSynopsis` varchar(500) NOT NULL,
-  `MovieGenre` varchar(100) NOT NULL,
-  `MovieLanguage` varchar(100) NOT NULL,
-  `MovieDuration` varchar(50) NOT NULL,
-  `MovieDate` date NOT NULL,
-  `MPARating` varchar(50) NOT NULL,
-  `MovieImage` varchar(200) NOT NULL,
-  `MovieCast` varchar(200) NOT NULL,
-  `MovieDirector` varchar(100) NOT NULL,
-  `MovieStatus` varchar(100) NOT NULL DEFAULT 'Active'
+  `movie_id` int(10) NOT NULL,
+  `movieTitle` varchar(200) NOT NULL,
+  `movieSynopsis` varchar(500) NOT NULL,
+  `movieGenre` varchar(100) NOT NULL,
+  `movieLanguage` varchar(100) NOT NULL,
+  `movieDuration` varchar(50) NOT NULL,
+  `mpaRating` varchar(50) NOT NULL,
+  `movieImage` varchar(200) NOT NULL,
+  `movieCast` varchar(200) NOT NULL,
+  `movieDirector` varchar(100) NOT NULL,
+  `movieStatus` varchar(100) NOT NULL DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `movie`
 --
 
-INSERT INTO `movie` (`MovieID`, `MovieTitle`, `MovieSynopsis`, `MovieGenre`, `MovieLanguage`, `MovieDuration`, `MovieDate`, `MPARating`, `MovieImage`, `MovieCast`, `MovieDirector`, `MovieStatus`) VALUES
-(1, 'Dungeons & Dragons (English Sub) PG13', 'A charming thief and a band of unlikely adventurers undertake an epic heist to retrieve a lost relic, but things go dangerously awry when they run afoul of the wrong people. Dungeons & Dragons: Honor Among Thieves brings the rich world and playful spirit of the legendary roleplaying game to the big screen in a hilarious and action-packed adventure.', 'Action, Fantasy', 'English', '134 mins', '2023-05-08', 'PG13 - Some Violence', '../Images/dnd.jpeg', 'Chris Pine, Hugh Grant, Michelle Rodriguez, Sophia Lillis , Justice Smith, Rege- Jean Page', 'Jonathan M. Goldstein, John Francis Daley', 'Active'),
-(2, 'John Wick: Chapter 4', 'John Wick (Keanu Reeves) uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.', 'Action', 'English', '170 mins', '2023-05-08', 'M18 - Violence', '../Images/johnwick4.jpeg', 'Donnie Yen , Scott Adkins, Keanu Reeves, Laurence Fishburne , Bill Skarsgard, Lance Reddick, Hiroyuki Sanada', 'Chad Stahelski', 'Active'),
-(3, 'The Super Mario Bros. Movie&#40;English Sub&#41;', 'From Nintendo and Illumination comes a new animated film based on the world of Super Mario Bros.', 'Animation', 'English', '92 mins', '2023-05-08', 'PG', '../Images/mario.jpeg', 'Jack Black, Seth Rogen, Chris Pratt, Keegan Michael Key, Charlie Day , Anya Taylor-Joy, Michael Jelenic, Fred Armisen, Kevin Michael Richardson, Sebastian Maniscalco', 'Aaron Horvath', 'Active'),
-(4, 'Suzume', 'As the skies turn red and the planet trembles, Japan stands on the brink of disaster. However, a determined teenager named Suzume sets out on a mission to save her country. Able to see supernatural forces that others cannot, it\'s up to her to close the mysterious doors that are spreading chaos across the land. A perilous journey awaits as the fate of Japan rests on her shoulders.', 'Anime', 'Japanese', '122 mins', '2023-05-10', 'PG', '../Images/suzume.jpeg', 'Nanoka Hara, Hokuto Matsumura, Eri Fukatsu', 'Makoto Shinkai', 'Active'),
-(5, 'Renfield', 'Evil doesn\'t span eternity without a little help.\r\n\r\nIn this modern monster tale of Dracula\'s loyal servant, Nicholas Hoult stars as Renfield, the tortured aide to history\'s most narcissistic boss, Dracula. Renfield is forced to procure his master\'s prey and do his every bidding, no matter how debased. But now, after centuries of servitude, Renfield is ready to see if there\'s a life outside the shadow of The Prince of Darkness. If only he can figure out how to end his codependency.', 'Comedy', 'English', '93 mins', '2023-05-17', 'M18 - Violence and Gore', '../Images/renfield.jpeg', 'Nicholas Hoult, Nicolas Cage, Awkwafina, Ben Schwartz, Adrian Martinez', 'Chris McKay', 'Active'),
-(6, 'Guardians of the Galaxy Vol. 3', 'In Marvel Studios \"Guardians of the Galaxy Vol. 3\" our beloved band of misfits are looking a bit different these days. Peter Quill, still reeling from the loss of Gamora, must rally his team around him to defend the universe along with protecting one of their own. A mission that, if not completed successfully, could quite possibly lead to the end of the Guardians as we know them.', 'Sci-Fi', 'English', '150 mins', '2023-05-18', 'PG13 - Some Violence and Coarse Language', '../Images/gotg3.jpeg', 'Bradley Cooper, Vin Diesel, Zoe Saldana, Chris Pratt, Dave Bautista , Karen Gillan', 'James Gunn', 'Active');
+INSERT INTO `movie` (`movie_id`, `movieTitle`, `movieSynopsis`, `movieGenre`, `movieLanguage`, `movieDuration`, `mpaRating`, `movieImage`, `movieCast`, `movieDirector`, `movieStatus`) VALUES
+(1, 'Dungeons & Dragons (English Sub) PG13', 'A charming thief and a band of unlikely adventurers undertake an epic heist to retrieve a lost relic, but things go dangerously awry when they run afoul of the wrong people. Dungeons & Dragons: Honor Among Thieves brings the rich world and playful spirit of the legendary roleplaying game to the big screen in a hilarious and action-packed adventure.', 'Action, Fantasy', 'English', '134 mins', 'PG13 - Some Violence', '../Images/dnd.jpeg', 'Chris Pine, Hugh Grant, Michelle Rodriguez, Sophia Lillis , Justice Smith, Rege- Jean Page', 'Jonathan M. Goldstein, John Francis Daley', 'Active'),
+(2, 'John Wick: Chapter 4', 'John Wick (Keanu Reeves) uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.', 'Action', 'English', '170 mins', 'M18 - Violence', '../Images/johnwick4.jpeg', 'Donnie Yen , Scott Adkins, Keanu Reeves, Laurence Fishburne , Bill Skarsgard, Lance Reddick, Hiroyuki Sanada', 'Chad Stahelski', 'Active'),
+(3, 'The Super Mario Bros. Movie&#40;English Sub&#41;', 'From Nintendo and Illumination comes a new animated film based on the world of Super Mario Bros.', 'Animation', 'English', '92 mins', 'PG', '../Images/mario.jpeg', 'Jack Black, Seth Rogen, Chris Pratt, Keegan Michael Key, Charlie Day , Anya Taylor-Joy, Michael Jelenic, Fred Armisen, Kevin Michael Richardson, Sebastian Maniscalco', 'Aaron Horvath', 'Active'),
+(4, 'Suzume', 'As the skies turn red and the planet trembles, Japan stands on the brink of disaster. However, a determined teenager named Suzume sets out on a mission to save her country. Able to see supernatural forces that others cannot, it\'s up to her to close the mysterious doors that are spreading chaos across the land. A perilous journey awaits as the fate of Japan rests on her shoulders.', 'Anime', 'Japanese', '122 mins', 'PG', '../Images/suzume.jpeg', 'Nanoka Hara, Hokuto Matsumura, Eri Fukatsu', 'Makoto Shinkai', 'Active'),
+(5, 'Renfield', 'Evil doesn\'t span eternity without a little help.\r\n\r\nIn this modern monster tale of Dracula\'s loyal servant, Nicholas Hoult stars as Renfield, the tortured aide to history\'s most narcissistic boss, Dracula. Renfield is forced to procure his master\'s prey and do his every bidding, no matter how debased. But now, after centuries of servitude, Renfield is ready to see if there\'s a life outside the shadow of The Prince of Darkness. If only he can figure out how to end his codependency.', 'Comedy', 'English', '93 mins', 'M18 - Violence and Gore', '../Images/renfield.jpeg', 'Nicholas Hoult, Nicolas Cage, Awkwafina, Ben Schwartz, Adrian Martinez', 'Chris McKay', 'Active'),
+(6, 'Guardians of the Galaxy Vol. 3', 'In Marvel Studios \"Guardians of the Galaxy Vol. 3\" our beloved band of misfits are looking a bit different these days. Peter Quill, still reeling from the loss of Gamora, must rally his team around him to defend the universe along with protecting one of their own. A mission that, if not completed successfully, could quite possibly lead to the end of the Guardians as we know them.', 'Sci-Fi', 'English', '150 mins', 'PG13 - Some Violence and Coarse Language', '../Images/gotg3.jpeg', 'Bradley Cooper, Vin Diesel, Zoe Saldana, Chris Pratt, Dave Bautista , Karen Gillan', 'James Gunn', 'Active');
 
 -- --------------------------------------------------------
 
@@ -149,16 +153,16 @@ INSERT INTO `movie` (`MovieID`, `MovieTitle`, `MovieSynopsis`, `MovieGenre`, `Mo
 --
 
 CREATE TABLE `ticket` (
-  `TicketID` int(10) NOT NULL,
-  `TicketPrice` int(100) NOT NULL,
-  `Ticket_cust_profile` varchar(100) NOT NULL
+  `ticket_id` int(10) NOT NULL,
+  `ticket_price` int(100) NOT NULL,
+  `ticket_cust_profile` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `ticket`
 --
 
-INSERT INTO `ticket` (`TicketID`, `TicketPrice`, `Ticket_cust_profile`) VALUES
+INSERT INTO `ticket` (`ticket_id`, `ticket_price`, `ticket_cust_profile`) VALUES
 (1, 5, 'Senior Citizen(Above 65yo)'),
 (2, 7, 'Student'),
 (3, 4, '	\r\nChild(Below 8yo)'),
@@ -214,14 +218,16 @@ INSERT INTO `user` (`user_id`, `username`, `email`, `password`, `cust_profile`, 
 (26, 'JohnGreen', 'johngreen@gmail.com', '792a62cd83eb4226d2564062e0c17697', 'Student', 'Customer', '2023-05-07', 'Active'),
 (27, 'AdamWhite', 'adamwhite123@gmail.com', 'a1Bz20ydqelm8m1wql8e4736afc2dc319d59c2cbd5e64c21b6', 'Senior Citizen(Above 65yo)', 'Customer', '2023-05-07', 'Active'),
 (28, 'Lisa1234', 'lisa1234@hotmail.com', '77424d0dd227f0ef8eecb54da4252be8', 'Child(Below 8yo)', 'Customer', '2023-05-07', 'Active'),
-(29, 'Mary00', 'mary00@gmail.com', '399f7212965e92ad6139e4521ed18acc', 'Adult', 'Customer', '2023-05-07', 'Active'),
+(29, 'Mary00', 'mary00@gmail.com', 'a1Bz20ydqelm8m1wql81dc9bdb52d04dc20036dbd8313ed055', 'Adult', 'User Admin', '2023-05-07', 'Active'),
 (30, 'Mary01', 'mary01@gmail.com', '94cfc6afee347a9aa2bde465402f8a79', 'Adult', 'Customer', '2023-05-07', 'Active'),
 (37, 'test2', 'test2@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'Adult', 'Customer', '0000-00-00', 'Active'),
 (45, 'cinemaOwner1', 'cinemaOwner1@gmail.com', 'a1Bz20ydqelm8m1wql760c0d63da5d52af25d1c6175b094ae7', '', 'Cinema Owner', '0000-00-00', 'Active'),
 (46, 'cinemaManager123', 'cinemaManager12@gmail.com', 'a1Bz20ydqelm8m1wql760c0d63da5d52af25d1c6175b094ae7', '', 'Cinema Manager', '0000-00-00', 'Active'),
 (47, 'useradmin22', 'useradmin2@gmail.com', 'a1Bz20ydqelm8m1wqlec6a6536ca304edf844d1d248a4f08dc', '', 'User Admin', '0000-00-00', 'Active'),
 (48, 'cinemaOwner123', 'cinemaOwner123@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '', 'Cinema Owner', '0000-00-00', 'Active'),
-(49, 'cinemaManager1234', 'cinemaManager1234@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '', 'Cinema Manager', '0000-00-00', 'Active');
+(49, 'cinemaManager1234', 'cinemaManager1234@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '', 'Cinema Manager', '0000-00-00', 'Active'),
+(50, 'bbt1234', 'bbt123@gmail.com', 'a1Bz20ydqelm8m1wql81dc9bdb52d04dc20036dbd8313ed055', '', 'User Admin', '0000-00-00', 'Active'),
+(51, 'testCinemaManager', 'testCinemaManager1234@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '', 'Cinema Manager', '0000-00-00', 'Active');
 
 -- --------------------------------------------------------
 
@@ -241,7 +247,7 @@ CREATE TABLE `userprofile` (
 
 INSERT INTO `userprofile` (`userProfile_ID`, `upName`, `profile_status`) VALUES
 (1, 'Cinema Owner', 'Active'),
-(2, 'Cinema Manager', 'Active'),
+(2, 'Cinema Manager1', 'Active'),
 (3, 'User Admin', 'Active'),
 (4, 'Customer', 'Active'),
 (5, 'test', 'Active');
@@ -255,13 +261,15 @@ INSERT INTO `userprofile` (`userProfile_ID`, `upName`, `profile_status`) VALUES
 --
 ALTER TABLE `booking`
   ADD PRIMARY KEY (`booking_id`),
-  ADD KEY `userBooked` (`userBooked`);
+  ADD KEY `userBooked` (`userBooked`),
+  ADD KEY `fk_RoomNo` (`roomNo`);
 
 --
 -- Indexes for table `cinema`
 --
 ALTER TABLE `cinema`
-  ADD PRIMARY KEY (`RoomNo`);
+  ADD PRIMARY KEY (`roomNo`),
+  ADD KEY `fk_movieID` (`movie_id`);
 
 --
 -- Indexes for table `combobooked`
@@ -275,19 +283,19 @@ ALTER TABLE `combobooked`
 -- Indexes for table `fnb`
 --
 ALTER TABLE `fnb`
-  ADD PRIMARY KEY (`ComboID`);
+  ADD PRIMARY KEY (`comboID`);
 
 --
 -- Indexes for table `movie`
 --
 ALTER TABLE `movie`
-  ADD PRIMARY KEY (`MovieID`);
+  ADD PRIMARY KEY (`movie_id`);
 
 --
 -- Indexes for table `ticket`
 --
 ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`TicketID`);
+  ADD PRIMARY KEY (`ticket_id`);
 
 --
 -- Indexes for table `ticketbooked`
@@ -323,7 +331,7 @@ ALTER TABLE `booking`
 -- AUTO_INCREMENT for table `cinema`
 --
 ALTER TABLE `cinema`
-  MODIFY `RoomNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `roomNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `combobooked`
@@ -335,19 +343,19 @@ ALTER TABLE `combobooked`
 -- AUTO_INCREMENT for table `fnb`
 --
 ALTER TABLE `fnb`
-  MODIFY `ComboID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `comboID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `MovieID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `movie_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `ticket`
 --
 ALTER TABLE `ticket`
-  MODIFY `TicketID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ticket_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `ticketbooked`
@@ -359,7 +367,7 @@ ALTER TABLE `ticketbooked`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `userprofile`
@@ -375,7 +383,14 @@ ALTER TABLE `userprofile`
 -- Constraints for table `booking`
 --
 ALTER TABLE `booking`
+  ADD CONSTRAINT `fk_RoomNo` FOREIGN KEY (`roomNo`) REFERENCES `cinema` (`roomNo`),
   ADD CONSTRAINT `userBooked` FOREIGN KEY (`userBooked`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `cinema`
+--
+ALTER TABLE `cinema`
+  ADD CONSTRAINT `fk_movieID` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`);
 
 --
 -- Constraints for table `combobooked`
@@ -388,7 +403,7 @@ ALTER TABLE `combobooked`
 -- Constraints for table `ticketbooked`
 --
 ALTER TABLE `ticketbooked`
-  ADD CONSTRAINT `fk_ticket_id` FOREIGN KEY (`ticket_id`) REFERENCES `ticket` (`TicketID`),
+  ADD CONSTRAINT `fk_ticket_id` FOREIGN KEY (`ticket_id`) REFERENCES `ticket` (`ticket_id`),
   ADD CONSTRAINT `fk_ticketbooked_booking` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`booking_id`) ON DELETE CASCADE;
 COMMIT;
 
